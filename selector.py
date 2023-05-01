e0e1f8d0a78ec46fcc8dab82285063821e712f43
rep87 <bangsioni@gmail.com>
2023-05-01 14:42:59 +0900

    feat : Done get lucky num

--- selector.py
+++ selector.py
@@ -1,9 +1,15 @@
 import random
 
-def random_select():
-    numbers = random.sample(range(1, 46), 6)
-    return sorted(numbers, reverse=True)
+def random_select(n):
+    results = []
+    for _ in range(n):
+        numbers = random.sample(range(1, 46), 6)
+        results.append(sorted(numbers))
+    return results
 
-selected_numbers = random_select()
-print(selected_numbers)
+num_draws = int(input("Enter the number of times you want to pick six numbers: "))
+selected_numbers = random_select(num_draws)
+
+for i, numbers in enumerate(selected_numbers, 1):
+    print(f"Draw {i}: {numbers}")
 
