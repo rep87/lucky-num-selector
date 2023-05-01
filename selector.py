04b4c26128455c0cc21420ba06fae804cc69a5c2
rep87 <bangsioni@gmail.com>
2023-05-01 14:25:34 +0900

    숫자 내림차순 정렬

--- selector.py
+++ selector.py
@@ -1,7 +1,8 @@
 import random
 
 def random_select():
-    return random.sample(range(1, 46), 6)
+    numbers = random.sample(range(1, 46), 6)
+    return sorted(numbers, reverse=True)
 
 selected_numbers = random_select()
 print(selected_numbers)
